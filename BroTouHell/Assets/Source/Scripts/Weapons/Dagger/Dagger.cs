679db56d41143e72aca9d2844cec737d01d8979d
kryakikrya <gon4.egor@gmail.com>
2025-07-27 11:32:38 +0300

    Shield and dagger

---
+++ BroTouHell/Assets/Source/Scripts/Weapons/Dagger/Dagger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Dagger : WeaponGeneral
+{
+    [SerializeField] private float _scalePower = 10f;
+    [SerializeField] private float _baseRotationSpeedAmplification = 115f;
+
+    private void Start()
+    {
+        _playerStats.IncreaseRotationSpeed(_baseRotationSpeedAmplification);
+    }
+    protected override void ScaleStats()
+    {
+        _playerStats.IncreaseRotationSpeed(_scalePower);
+    }
+}
---
+++ BroTouHell/Assets/Source/Scripts/Weapons/Shield/Shield.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Shield : WeaponGeneral
+{
+    [SerializeField] private float _scalePower = 3f;
+    [SerializeField] private float _baseParryDamageAmplification = 100f;
+
+    private void Start()
+    {
+        _playerStats.IncreaseParryDamage(_baseParryDamageAmplification);
+    }
+    protected override void ScaleStats()
+    {
+        _playerStats.IncreasePlayerHealth(_scalePower);
+    }
+}
